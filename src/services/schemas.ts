c5b585abc61f416ea93a4ef95593183ce28f2a97
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-06-18 19:42:12 +0100

    changed import in yup-schema

--- src/services/schemas.ts
+++ src/services/schemas.ts
@@ -1,10 +1,10 @@
-import * as Yup from 'yup';
+import { object as YupObject, string as YupString } from 'yup';
 
-export const loginSchema = Yup.object().shape({
-  email: Yup.string()
+export const loginSchema = YupObject().shape({
+  email: YupString()
     .email('Invalid email format')
     .required('Email is required'),
-  password: Yup.string()
+  password: YupString()
     .min(7, 'password must be at least 7 characters!')
     .max(50, 'password must be less than 50 characters!')
     .required('password is required!')
@@ -14,12 +14,12 @@ export const loginSchema = Yup.object().shape({
     ),
 });
 
-export const registerSchema = Yup.object().shape({
-  name: Yup.string()
+export const registerSchema = YupObject().shape({
+  name: YupString()
     .min(3, 'Register name must be at least 3 characters!')
     .max(50, 'Register name must be less than 50 characters!')
     .required('Register name is required!'),
-  phone: Yup.string()
+  phone: YupString()
     .min(5, 'Phone number must be at least 5 characters!')
     .max(20, 'Phone number must be less than 20 characters!')
     .required('Phone number is required!')
@@ -27,10 +27,10 @@ export const registerSchema = Yup.object().shape({
       /^\+?(\d{1,3})?[- .]?\(?\d{3}\)?[- .]?\d{3}[- .]?\d{1,4}$/,
       'Password must include a lowercase letter, an uppercase letter, and a number'
     ),
-  email: Yup.string()
+  email: YupString()
     .email('Invalid email format')
     .required('Email is required'),
-  password: Yup.string()
+  password: YupString()
     .min(7, 'password must be at least 7 characters!')
     .max(50, 'password must be less than 50 characters!')
     .required('password is required!')
